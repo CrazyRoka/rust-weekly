6d97c069747e693ce868e50ab4049aec6721f88c
CrazyRoka <rokarostuk@gmail.com>
2021-07-31 22:30:20 +0300

    Added latest command

--- src/lib.rs
+++ src/lib.rs
@@ -10,8 +10,19 @@ static APP_USER_AGENT: &str = concat!(env!("CARGO_PKG_NAME"), "/", env!("CARGO_P
 
 #[derive(Debug)]
 pub struct ArticleLink {
-    url: String,
-    name: String,
+    pub url: String,
+    pub name: String,
+    pub download_url: String,
+}
+
+pub struct Article {
+    pub text: String,
+}
+
+impl Article {
+    fn new(text: String) -> Self {
+        Self { text }
+    }
 }
 
 #[derive(Deserialize, PartialEq)]
@@ -26,12 +37,15 @@ enum ContentType {
 struct GithubContentResponse {
     r#type: ContentType,
     name: String,
+    #[serde(rename = "html_url")]
     url: String,
+    download_url: Option<String>,
 }
 
 #[async_trait]
 pub trait ContentFetcher {
     async fn fetch_content(&self) -> Result<Vec<ArticleLink>>;
+    async fn fetch_article(&self, link: &ArticleLink) -> Result<Article>;
 }
 
 pub struct GithubContentFetcher {}
@@ -51,15 +65,28 @@ impl ContentFetcher for GithubContentFetcher {
             .user_agent(APP_USER_AGENT)
             .build()?;
         let response: Vec<GithubContentResponse> = client.get(url).send().await?.json().await?;
-
-        Ok(response
+        let mut links: Vec<ArticleLink> = response
             .into_iter()
             .filter(|item| item.r#type == ContentType::File)
             .filter(|item| item.name.ends_with("md") || item.name.ends_with("markdown"))
             .map(|item| ArticleLink {
                 url: item.url,
                 name: item.name,
+                download_url: item.download_url.expect("File should have download url"),
             })
-            .collect())
+            .collect();
+        links.sort_by(|a, b| a.name.cmp(&b.name).reverse());
+
+        Ok(links)
+    }
+
+    async fn fetch_article(&self, link: &ArticleLink) -> Result<Article> {
+        println!("{}", link.download_url.to_string());
+        let client = reqwest::Client::builder()
+            .user_agent(APP_USER_AGENT)
+            .build()?;
+        let response = client.get(&link.download_url).send().await?.text().await?;
+
+        Ok(Article::new(response))
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,79 @@
-use rust_weekly::{GithubContentFetcher, ContentFetcher};
+use clap::{App, Arg, SubCommand};
+use rust_weekly::{ContentFetcher, GithubContentFetcher};
+use termimad::MadSkin;
+
+const DEFAULT_ARTICLES_COUNT: usize = 5;
 
 #[tokio::main]
 async fn main() -> reqwest::Result<()> {
+    let matches = App::new("Rust Weekly CLI")
+        .version(env!("CARGO_PKG_VERSION"))
+        .author(env!("CARGO_PKG_AUTHORS"))
+        .about(env!("CARGO_PKG_DESCRIPTION"))
+        .subcommand(
+            SubCommand::with_name("articles")
+                .about("Prints recent articles")
+                .arg(
+                    Arg::with_name("count")
+                        .short("c")
+                        .takes_value(true)
+                        .multiple(false)
+                        .help(&format!(
+                            "print 'c' recent articles. Default to {}",
+                            DEFAULT_ARTICLES_COUNT
+                        )),
+                ),
+        )
+        .subcommand(SubCommand::with_name("latest").about("Prints latest article"))
+        .get_matches();
+
+    match matches.subcommand() {
+        ("articles", Some(args)) => {
+            let articles_count = match args.value_of("count") {
+                Some(count) => match count.parse() {
+                    Ok(value) if value > 0 => value,
+                    _ => {
+                        eprintln!(
+                            "Invalid count of articles: \"{}\". Expected a positive number",
+                            count
+                        );
+                        return Ok(());
+                    }
+                },
+                _ => DEFAULT_ARTICLES_COUNT,
+            };
+
+            print_articles_list(articles_count).await?;
+        }
+        ("latest", _) | _ => {
+            print_latest_article().await?
+        }
+    }
+
+    Ok(())
+}
+
+async fn print_articles_list(articles_count: usize) -> reqwest::Result<()> {
     let content_fetcher = GithubContentFetcher::new();
     let links = content_fetcher.fetch_content().await?;
 
-    for link in links {
-        println!("{:?}", link);
+    println!("{}", links.len());
+    for link in links.into_iter().take(articles_count) {
+        println!("[{}] {}", link.name, link.url);
     }
+
+    Ok(())
+}
+
+async fn print_latest_article() -> reqwest::Result<()> {
+    let content_fetcher = GithubContentFetcher::new();
+    let links = content_fetcher.fetch_content().await?;
+
+    let latest_link = links.get(0).expect("There should be at least one article");
+    let article = content_fetcher.fetch_article(latest_link).await?;
+
+    let skin = MadSkin::default();
+    skin.print_text(&article.text);
 
     Ok(())
 }
